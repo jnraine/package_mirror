38b2b1d53a5ece7da1ec8ada0004e99bcb732369
Jordan Raine <jnraine@gmail.com>
2013-03-24 21:45:57 -0700

    add nil check for installer_item_location
    If a package mirror object doesn't have an installer_item_location, send a 404.

--- app/controllers/mirrors_controller.rb
+++ app/controllers/mirrors_controller.rb
@@ -1,7 +1,7 @@
 class MirrorsController < ApplicationController
   def download
     @mirror = Mirror.get(params[:id])
-    if @mirror.present?
+    if @mirror.present? and @mirror.installer_item_location.present?
       send_file Mirror.packages_dir + @mirror.installer_item_location, :filename => @mirror.download_filename
       fresh_when :etag => @mirror, :last_modified => @mirror.created_at.utc, :public => true
     else
